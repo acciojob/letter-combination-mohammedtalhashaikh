3eb0902aa6f2e4935d6dca3397efb4939cd961e4
Talha Shaikh <talhashaikh6366@gmail.com>
2022-12-24 11:42:04 +0530

    solution

--- solution.js
+++ solution.js
@@ -1,5 +1,24 @@
 function letterCombinations(input_digit) {
   //Complete the function
+  const map = [0,1,'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+  const res = [];
+  const que = [];
+  let str ='';
+
+  while(que.length>0){
+    str = que[0];
+    que.shift();
+    if(str.length === input_digit.length){
+      res.push(str);
+    } else {
+      const n = Number(input_digit.charAt(str.length));
+      const value = map[n];
+      for(let i=0;i<val.length;i++){
+        que.push(str+val[i]);
+    }
+  }
+  return res;
 }
+}  
 
 module.exports = letterCombinations;
